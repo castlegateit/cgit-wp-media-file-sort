ff05f3e2389dd2adf6e344f30ff212bf3cd99115
John Hughes <john@castlegateit.co.uk>
2017-07-21 09:52:57 +0100

    Refactor

--- cgit-wp-media-file-sort.php
+++ cgit-wp-media-file-sort.php
@@ -5,38 +5,34 @@
 Plugin Name: Castlegate IT WP Media File Sort
 Plugin URI: http://github.com/castlegateit/cgit-wp-media-file-sort
 Description: Allow media library to be sorted by file name.
-Version: 1.0
+Version: 1.1
 Author: Castlegate IT
 Author URI: http://www.castlegateit.co.uk/
 License: MIT
 
-Based on <http://wordpress.stackexchange.com/questions/84030/add-file-name-column-to-media-library>
-with modifications to avoid naming collisions.
-
 */
 
-/**
- * Add to init action
- */
-add_action('admin_init', function() {
+if (!defined('ABSPATH')) {
+    wp_die('Access denied');
+}
 
-    // Add column to media library
-    add_filter('manage_media_columns', function($cols) {
-        $cols['filename'] = 'File name';
-        return $cols;
-    });
+/** Add column to media library */
+add_filter('manage_media_columns', function ($columns) {
+    $columns['file_name'] = 'File name';
 
-    // Display file names in column
-    //
-    // WordPress stores the file URL, not the file name
-    add_action('manage_media_custom_column', function($column_name, $id) {
-        $meta = wp_get_attachment_metadata($id);
-        echo substr(strrchr($meta['file'], '/' ), 1);
-    }, 10, 2);
+    return $columns;
+});
 
-    // Allow sorting by file name column
-    add_filter('manage_upload_sortable_columns', function($cols) {
-        $cols['filename'] = 'name';
-        return $cols;
-    });
+/** Display file names in new media library column */
+add_action('manage_media_custom_column', function ($name, $id) {
+    $meta = wp_get_attachment_metadata($id);
+
+    echo basename($meta['file']);
+}, 10, 2);
+
+/** Enable columns to be sorted by file name */
+add_filter('manage_upload_sortable_columns', function ($columns) {
+    $columns['file_name'] = 'name';
+
+    return $columns;
 });
